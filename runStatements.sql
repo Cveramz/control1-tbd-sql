288fb0a1603cc314f8275809cd6eba6c346f3d6e
CarlosVeRamz <83312809+Cveramz@users.noreply.github.com>
2023-09-19 22:35:33 -0300

    Carlos Vera - Query "tienda con menor empleados"

--- runStatements.sql
+++ runStatements.sql
@@ -25,3 +25,37 @@ INNER JOIN (
 INNER JOIN venta v ON t.num_tienda = v.num_tienda
 INNER JOIN prod_venta pv ON v.id_venta = pv.id_venta
 INNER JOIN producto p ON pv.id_producto = p.id_producto AND p.precio = subquery.precio_minimo;
+
+
+-- la tienda que tiene menos empleados
+
+SELECT
+    t.num_tienda,
+    t.alias AS nombre_tienda,
+    subquery.min_cantidad_empleados AS cantidad_empleados
+FROM
+    tienda t
+INNER JOIN (
+    SELECT
+        MIN(cantidad_empleados) AS min_cantidad_empleados
+    FROM (
+        SELECT
+            num_tienda,
+            COUNT(rut_empleado) AS cantidad_empleados
+        FROM
+            tienda_empleado
+        GROUP BY
+            num_tienda
+    ) empleados_por_tienda
+) subquery ON 1=1
+INNER JOIN (
+    SELECT
+        num_tienda,
+        COUNT(rut_empleado) AS cantidad_empleados
+    FROM
+        tienda_empleado
+    GROUP BY
+        num_tienda
+) empleados_por_tienda ON t.num_tienda = empleados_por_tienda.num_tienda
+WHERE
+    empleados_por_tienda.cantidad_empleados = subquery.min_cantidad_empleados;
